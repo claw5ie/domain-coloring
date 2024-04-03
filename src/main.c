6bbc1e86f9dab7ff730538fdece67eae7b555510
claw5ie <92851066+claw5ie@users.noreply.github.com>
2024-04-03 19:43:50 +0100

    Add ability to move around

--- src/main.c
+++ src/main.c
@@ -33,13 +33,13 @@ typedef double f64;
 
 #include "utils.c"
 
-int SCREEN_WIDTH = 800;
-int SCREEN_HEIGHT = 600;
+int g_screen_width = 800;
+int g_screen_height = 600;
 
-float LEFT = (-8.0);
-float RIGHT = 8.0;
-float BOTTOM = (-6.0);
-float TOP = 6.0;
+float g_left = (-8.0);
+float g_right = 8.0;
+float g_bottom = (-6.0);
+float g_top = 6.0;
 
 typedef struct GlfwContext GlfwContext;
 struct GlfwContext
@@ -50,59 +50,133 @@ struct GlfwContext
   GLint y_axis_loc;
   GLint transform_loc;
   GLint projection_loc;
+
+  bool is_lmb_pressed;
+
+  float x_mouse_pos;
+  float y_mouse_pos;
 };
 
 void
-update_coordinate_system(GlfwContext *ctx)
+update_coordinate_system(GlfwContext *ctx, float left, float right, float bottom, float top)
 {
   glUseProgram(ctx->program);
 
-  glUniform2f(ctx->screen_loc, SCREEN_WIDTH, SCREEN_HEIGHT);
-  glUniform2f(ctx->x_axis_loc, LEFT, BOTTOM);
-  glUniform2f(ctx->y_axis_loc, RIGHT, TOP);
+  glUniform2f(ctx->screen_loc, g_screen_width, g_screen_height);
+  glUniform2f(ctx->x_axis_loc, left, bottom);
+  glUniform2f(ctx->y_axis_loc, right, top);
 
   {
     float matrix[4][4] = {
-      { fabsf(RIGHT - LEFT), 0, 0, 0 },
-      { 0, fabsf(TOP - BOTTOM), 0, 0 },
+      { fabsf(right - left), 0, 0, 0 },
+      { 0, fabsf(top - bottom), 0, 0 },
       { 0, 0, 1, 0 },
-      { LEFT, BOTTOM, 0, 1 },
+      { left, bottom, 0, 1 },
     };
     glUniformMatrix4fv(ctx->transform_loc, 1, GL_FALSE, (float *)matrix);
   }
 
   {
     float matrix[4][4] = {
-      { 2.0 / (RIGHT - LEFT), 0, 0, 0 },
-      { 0, 2.0 / (TOP - BOTTOM), 0, 0 },
+      { 2.0 / (right - left), 0, 0, 0 },
+      { 0, 2.0 / (top - bottom), 0, 0 },
       { 0, 0, 1, 0 },
-      { -(RIGHT + LEFT) / (RIGHT - LEFT), -(TOP + BOTTOM) / (TOP - BOTTOM), 0, 1 },
+      { -(right + left) / (right - left), -(top + bottom) / (top - bottom), 0, 1 },
     };
     glUniformMatrix4fv(ctx->projection_loc, 1, GL_FALSE, (float *)matrix);
   }
 }
 
 void
+to_local_coordinates(float x, float y, float *x_dst, float *y_dst)
+{
+  float xx = x * (g_right - g_left) / g_screen_width;
+  xx += g_left;
+  float yy = y * (g_bottom - g_top) / g_screen_height;
+  yy += g_top;
+
+  *x_dst = xx;
+  *y_dst = yy;
+}
+
+void
+get_cursor_pos(GLFWwindow *window, float *x_dst, float *y_dst)
+{
+  double x, y;
+  glfwGetCursorPos(window, &x, &y);
+  to_local_coordinates(x, y, x_dst, y_dst);
+}
+
+void
 framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-  float scale = (RIGHT - LEFT) / (TOP - BOTTOM) * ((float)height / width);
-  float offset = (TOP + BOTTOM) / 2;
+  float scale = (g_right - g_left) / (g_top - g_bottom) * ((float)height / width);
+  float offset = (g_top + g_bottom) / 2;
 
-  BOTTOM -= offset;
-  TOP -= offset;
-  BOTTOM *= scale;
-  TOP *= scale;
-  BOTTOM += offset;
-  TOP += offset;
+  g_bottom -= offset;
+  g_top -= offset;
+  g_bottom *= scale;
+  g_top *= scale;
+  g_bottom += offset;
+  g_top += offset;
 
-  SCREEN_WIDTH = width;
-  SCREEN_HEIGHT = height;
+  g_screen_width = width;
+  g_screen_height = height;
 
   glViewport(0, 0, width, height);
 
   GlfwContext *ctx = glfwGetWindowUserPointer(window);
   assert(ctx);
-  update_coordinate_system(ctx);
+  update_coordinate_system(ctx, g_left, g_right, g_bottom, g_top);
+}
+
+void
+mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+{
+  (void)mods;
+
+  GlfwContext *ctx = glfwGetWindowUserPointer(window);
+  assert(ctx);
+
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+      get_cursor_pos(window, &ctx->x_mouse_pos, &ctx->y_mouse_pos);
+      ctx->is_lmb_pressed = true;
+    }
+  else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+      float x_offset = 0, y_offset = 0;
+      get_cursor_pos(window, &x_offset, &y_offset);
+
+      x_offset -= ctx->x_mouse_pos;
+      y_offset -= ctx->y_mouse_pos;
+
+      g_left -= x_offset;
+      g_right -= x_offset;
+      g_bottom -= y_offset;
+      g_top -= y_offset;
+
+      update_coordinate_system(ctx, g_left, g_right, g_bottom, g_top);
+
+      ctx->is_lmb_pressed = false;
+    }
+}
+
+void
+cursor_pos_callback(GLFWwindow *window, double x, double y)
+{
+  GlfwContext *ctx = glfwGetWindowUserPointer(window);
+  assert(ctx);
+
+  if (ctx->is_lmb_pressed)
+    {
+      float x_offset = 0, y_offset = 0;
+      to_local_coordinates(x, y, &x_offset, &y_offset);
+      x_offset -= ctx->x_mouse_pos;
+      y_offset -= ctx->y_mouse_pos;
+
+      update_coordinate_system(ctx, g_left - x_offset, g_right - x_offset, g_bottom - y_offset, g_top - y_offset);
+    }
 }
 
 int
@@ -115,7 +189,7 @@ main(void)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello, World!", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(g_screen_width, g_screen_height, "Hello, World!", NULL, NULL);
   if (!window)
     exit(EXIT_FAILURE);
 
@@ -128,6 +202,8 @@ main(void)
   puts((const char *)glGetString(GL_RENDERER));
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetMouseButtonCallback(window, mouse_button_callback);
+  glfwSetCursorPosCallback(window, cursor_pos_callback);
 
   float quad[] = { 0, 0,
                    1, 0,
@@ -178,11 +254,12 @@ main(void)
     .y_axis_loc = y_axis_loc,
     .transform_loc = transform_loc,
     .projection_loc = projection_loc,
+    .is_lmb_pressed = false,
   };
 
   glfwSetWindowUserPointer(window, &ctx);
 
-  update_coordinate_system(&ctx);
+  update_coordinate_system(&ctx, g_left, g_right, g_bottom, g_top);
 
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
